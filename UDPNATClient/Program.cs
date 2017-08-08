1109309bc73eb86b0a8ea288cb9d44ff713cd5fb
平斌 <18728856@qq.com>
2017-08-08 16:21:55 +0800

    添加项目文件。

---
+++ UDPNATClient/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPNATClient
+{
+    class Program
+    {
+        static UdpClient mUdpClient;
+
+        static string RemoteClientIP;
+
+        static int RemoteClientPort;
+        static bool isTeacher = System.Configuration.ConfigurationManager.AppSettings["ProgramType"] == "TECH" ? true : false;
+        static bool autoSend = System.Configuration.ConfigurationManager.AppSettings["AutoSend"] == "1" ? true : false;
+        static void Main(string[] args)
+        {
+            try
+            {
+
+
+
+
+                // Console.WriteLine("请输入服务器IP");
+                string IP = System.Configuration.ConfigurationManager.AppSettings["ServerIP"];
+                //  Console.WriteLine("请输入服务器端口");
+                int Port = int.Parse(System.Configuration.ConfigurationManager.AppSettings["ServerPort"]);
+                IPEndPoint fLocalIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+
+                //IPEndPoint LocalPt = new IPEndPoint(Dns.GetHostEntry(Dns.GetHostName()).AddressList[0], 0);
+                //mUdpClient = new UdpClient();
+                //mUdpClient.ExclusiveAddressUse = false;
+                //mUdpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                //mUdpClient.Client.Bind(LocalPt);
+
+
+                mUdpClient = new UdpClient(fLocalIPEndPoint);
+                uint IOC_IN = 0x80000000;
+                uint IOC_VENDOR = 0x18000000;
+                uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+            //    IOControlCode.DataToRead
+                mUdpClient.Client.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+                mUdpClient.Client.ReceiveBufferSize = 4096;
+                //    mUdpClient.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+                RunReceive();
+                SendUDP((isTeacher ? "hello" : "hello"), IP, Port);
+                //  byte[] fHelloData = Encoding.UTF8.GetBytes("hello");
+                //  mUdpClient.Send(fHelloData, fHelloData.Length, IP, Port);
+
+                while (true)
+                {
+                    string Content = Console.ReadLine();
+                    if (RemoteClientIP != null)
+                    {
+                        if (Content == "pic")
+                        {
+                            Screen screen = new Screen();
+                            byte[] fData = screen.GetScreenCapture();
+                            mUdpClient.Send(fData, fData.Length, RemoteClientIP, RemoteClientPort);
+                            Console.WriteLine("发送地址 " + RemoteClientIP + ":" + RemoteClientPort + "   图片长度:" + fData.Length);
+                        }
+                        else if (Content.StartsWith("big"))
+                        {
+                            int length = Convert.ToInt32(Content.Substring(3));
+                            string x = "";
+                            for (int i = 0; i < length; i++)
+                            {
+                                x += "x";
+                            }
+                            byte[] fData = Encoding.UTF8.GetBytes(x);
+                            mUdpClient.Send(fData, fData.Length, RemoteClientIP, RemoteClientPort);
+                            Console.WriteLine("发送地址 " + RemoteClientIP + ":" + RemoteClientPort + "   大数据长度:" + fData.Length);
+                        }
+                        else
+                        {
+                            SendUDP(Content, RemoteClientIP, RemoteClientPort);
+                        }
+                        //  byte[] fData = Encoding.UTF8.GetBytes(Content);
+                        //   mUdpClient.Send(fData, fData.Length, RemoteClientIP, RemoteClientPort);
+                        //   Console.WriteLine("发送地址" + RemoteClientIP + ":" + RemoteClientPort + "   内容:" + Content);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UDP服务启动失败,监听端口：6000");
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadKey();
+        }
+
+        private static void SendUDP(string content, string ip, int port)
+        {
+            byte[] fData = Encoding.UTF8.GetBytes(content);
+            mUdpClient.Send(fData, fData.Length, ip, port);
+            Console.WriteLine("发送地址" + ip + ":" + port + "   内容:" + content);
+        }
+
+
+        static void RunReceive()
+        {
+            Thread th = new Thread(() =>
+            {
+
+
+
+                while (true)
+                {
+                    IPEndPoint fClientIPEndPoint = null;
+                    byte[] fData = mUdpClient.Receive(ref fClientIPEndPoint);
+                    if (fData.Length > 0)
+                    {//数据接收成功,放入缓存
+                        if (fData.Length < 100)
+                        {
+                            string fContent = Encoding.UTF8.GetString(fData);
+                            Console.WriteLine("收到信息：" + fContent);
+                            string fIPAddress = fClientIPEndPoint.Address.ToString() + ":" + fClientIPEndPoint.Port;
+                            Console.WriteLine(fIPAddress + ":" + fContent);
+                            if (fContent.StartsWith("/"))
+                            {
+                                string[] fClientData = fContent.Split(':');
+                                RemoteClientIP = fClientData[0].Substring(1);
+                                RemoteClientPort = int.Parse(fClientData[1]);
+                                if (autoSend)
+                                {
+                                    SendUDP("hello", RemoteClientIP, RemoteClientPort);
+                                }
+                                Console.WriteLine("远程客户端已穿透，请输入需要发送的信息");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("收到大数据：长度" + fData.Length);
+                        }
+
+                    }
+                    Thread.Sleep(200);
+                }
+
+
+
+            });
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        /// <summary>
+        /// 异步接收数据
+        /// </summary>
+        /// <param name="ar"></param>
+        static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                if (mUdpClient.Client != null)
+                {
+                    IPEndPoint fClientIPEndPoint = null;
+                    byte[] fData = mUdpClient.EndReceive(ar, ref fClientIPEndPoint);
+                    if (fData.Length > 0)
+                    {//数据接收成功,放入缓存
+                        string fContent = Encoding.UTF8.GetString(fData);
+                        Console.WriteLine("收到信息：" + fContent);
+                        string fIPAddress = fClientIPEndPoint.Address.ToString() + ":" + fClientIPEndPoint.Port;
+                        Console.WriteLine(fIPAddress + ":" + fContent);
+                        if (fContent.StartsWith("/"))
+                        {
+                            string[] fClientData = fContent.Split(':');
+                            RemoteClientIP = fClientData[0].Substring(1);
+                            RemoteClientPort = int.Parse(fClientData[1]);
+                            if (autoSend)
+                            {
+                                SendUDP("hello", RemoteClientIP, RemoteClientPort);
+                            }
+                            Console.WriteLine("远程客户端已穿透，请输入需要发送的信息");
+                        }
+                        //else
+                        //{
+                        //    Console.WriteLine(fContent);
+                        //}
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                try
+                {
+                    mUdpClient.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ UDPNATServer/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace UDPNATServer
+{
+    class Program
+    {
+        static UdpClient mUdpReciver;
+
+        static List<string> ClientIPPort = new List<string>();
+        static int port = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["port"]);
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPEndPoint fLocalIPEndPoint = new IPEndPoint(IPAddress.Any, port);
+                mUdpReciver = new UdpClient(fLocalIPEndPoint);
+                uint IOC_IN = 0x80000000;
+                uint IOC_VENDOR = 0x18000000;
+                uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+                mUdpReciver.Client.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
+                //  mUdpReciver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+                RunReceive();
+                Console.WriteLine("UDP服务启动成功,监听端口：" + port);
+                while (true)
+                {
+                    string Content = Console.ReadLine();
+                    foreach (string item in ClientIPPort)
+                    {
+                        string[] ClientAddress = item.Split(':');
+                        byte[] fData = Encoding.UTF8.GetBytes(Content);
+                        mUdpReciver.Send(fData, fData.Length, ClientAddress[0], int.Parse(ClientAddress[1]));
+                        Console.WriteLine("发送地址" + ClientAddress[0] + ":" + ClientAddress[1] + "   内容:" + Content);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UDP服务启动失败,监听端口：" + port);
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static object obLock = new object();
+
+        static void RunReceive()
+        {
+            Thread th = new Thread(() =>
+            {
+
+                lock (obLock)
+                {
+                    IPEndPoint fClientIPEndPoint = null;
+                    while (true)
+                    {
+                        byte[] fData = mUdpReciver.Receive(ref fClientIPEndPoint);
+                        if (fData.Length > 0)
+                        {//数据接收成功,放入缓存
+                            string fIPAddress = fClientIPEndPoint.Address.ToString() + ":" + fClientIPEndPoint.Port;
+                            string fContent = Encoding.UTF8.GetString(fData);
+                            Console.WriteLine(fIPAddress + ":" + fContent);
+                            if (fContent == "hello")
+                            {
+                                if (ClientIPPort.Count < 2)
+                                {
+                                    if (ClientIPPort.Contains(fIPAddress) == false)
+                                    {
+                                        ClientIPPort.Add(fIPAddress);
+                                        Console.WriteLine("客户端注册成功,当前已注册客户端数量：" + ClientIPPort.Count);
+                                        if (ClientIPPort.Count == 2)
+                                        {
+                                            //将客户端1的地址发送给客户端2
+                                            //  byte[] Client1AddressData = Encoding.UTF8.GetBytes("/" + ClientIPPort[0]);
+                                            string[] Client1Address = ClientIPPort[1].Split(':');
+                                            SendUDP("/" + ClientIPPort[0], Client1Address[0], int.Parse(Client1Address[1]));
+                                            //   mUdpReciver.Send(Client1AddressData, Client1AddressData.Length, Client1Address[0], int.Parse(Client1Address[1]));
+                                            Thread.Sleep(1000);
+                                            //将客户端2的地址发送给客户端1
+                                            string[] Client2Address = ClientIPPort[0].Split(':');
+                                            SendUDP("/" + ClientIPPort[1], Client2Address[0], int.Parse(Client2Address[1]));
+                                            //  byte[] Client2AddressData = Encoding.UTF8.GetBytes("/" + ClientIPPort[1]);
+                                            //  string[] Client2Address = ClientIPPort[0].Split(':');
+                                            // mUdpReciver.Send(Client2AddressData, Client2AddressData.Length, Client2Address[0], int.Parse(Client2Address[1]));
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("客户端已满");
+                                }
+                            }
+                        }
+                        Thread.Sleep(200);
+                    }
+                }
+
+
+            });
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private static void SendUDP(string content, string ip, int port)
+        {
+            byte[] fData = Encoding.UTF8.GetBytes(content);
+            mUdpReciver.Send(fData, fData.Length, ip, port);
+            Console.WriteLine("发送地址" + ip + ":" + port + "   内容:" + content);
+        }
+
+        ///// <summary>
+        ///// 异步接收数据
+        ///// </summary>
+        ///// <param name="ar"></param>
+        //static void ReceiveCallback(IAsyncResult ar)
+        //{
+        //    try
+        //    {
+        //        if (mUdpReciver.Client != null)
+        //        {
+        //            IPEndPoint fClientIPEndPoint = null;
+        //            byte[] fData = mUdpReciver.EndReceive(ar, ref fClientIPEndPoint);
+        //            if (fData.Length > 0)
+        //            {//数据接收成功,放入缓存
+        //                string fIPAddress = fClientIPEndPoint.Address.ToString() + ":" + fClientIPEndPoint.Port;
+        //                string fContent = Encoding.UTF8.GetString(fData);
+        //                Console.WriteLine(fIPAddress + ":" + fContent);
+        //                if (fContent == "hello")
+        //                {
+        //                    if (ClientIPPort.Count < 2)
+        //                    {
+        //                        if (ClientIPPort.Contains(fIPAddress) == false)
+        //                        {
+        //                            ClientIPPort.Add(fIPAddress);
+        //                            Console.WriteLine("客户端注册成功,当前已注册客户端数量：" + ClientIPPort.Count);
+        //                            if (ClientIPPort.Count == 2)
+        //                            {
+        //                                //将客户端1的地址发送给客户端2
+        //                                byte[] Client1AddressData = Encoding.UTF8.GetBytes("/" + ClientIPPort[0]);
+        //                                string[] Client1Address = ClientIPPort[1].Split(':');
+        //                                mUdpReciver.Send(Client1AddressData, Client1AddressData.Length, Client1Address[0], int.Parse(Client1Address[1]));
+        //                                Thread.Sleep(2000);
+        //                                //将客户端2的地址发送给客户端1
+        //                                byte[] Client2AddressData = Encoding.UTF8.GetBytes("/" + ClientIPPort[1]);
+        //                                string[] Client2Address = ClientIPPort[0].Split(':');
+        //                                mUdpReciver.Send(Client2AddressData, Client2AddressData.Length, Client2Address[0], int.Parse(Client2Address[1]));
+        //                            }
+        //                        }
+        //                    }
+        //                    else
+        //                    {
+        //                        Console.WriteLine("客户端已满");
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.ToString());
+        //    }
+        //    finally
+        //    {
+        //        try
+        //        {
+        //            mUdpReciver.BeginReceive(new AsyncCallback(ReceiveCallback), null);
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Console.WriteLine(ex.ToString());
+        //        }
+        //    }
+        //}
+
+
+
+    }
+}
